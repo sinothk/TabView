ba748d06cbba14e29948ef8deb61ee0950b9c828
LiangYutao <sinothk@126.com>
2018-08-13 15:19:57 +0800

    修改未读提示尺寸！

---
+++ app/src/main/java/com/sinothk/tab/demo/UnreadTipFragment.java
@@ -0,0 +1,17 @@
+package com.sinothk.tab.demo;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * <pre>
+ *  创建:  LiangYT 2018/5/21/021 on 10:40
+ *  项目: TabViewLib
+ *  描述:
+ *  更新:
+ * <pre>
+ */
+public class Test1 extends Fragment {
+    public static Fragment newInstance(String s) {
+        return new Test1();
+    }
+}
